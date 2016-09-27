855f695b7648702bc8b4293aa36cf9bbb21e1b09
Diamantan <shokolp@gmail.com>
2016-09-28 01:38:27 +0200

    Basic view now returns minimal HTML

--- functional_test.py
+++ functional_test.py
@@ -43,4 +43,4 @@ class NewVistorTest(unittest.TestCase):
 		# Satisfied, she goes back to sleep
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+	unittest.main(exit=False, warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,21 @@
-from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
+
+
+
 # Create your tests here.
 class SmokeTest(TestCase):
 
 	def test_root_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct__html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
