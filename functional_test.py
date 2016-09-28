762640b9f07dae3668b5f48139014f03bbdfc699
Diamantan <shokolp@gmail.com>
2016-09-28 17:09:18 +0200

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
 
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 class NewVistorTest(unittest.TestCase):
 	def setUp(self):
@@ -18,16 +19,23 @@ class NewVistorTest(unittest.TestCase):
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 
 		# She is invited to enter a to-do item straight away
+		imputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(imputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
-
+		inputbox.send_keys('Buy peacock feathers')
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
+		inputbox.send_keys(Keys.ENTER)
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -41,6 +49,7 @@ class NewVistorTest(unittest.TestCase):
 		# She visits that URL - her to-do list is still there.
 
 		# Satisfied, she goes back to sleep
+		self.fail('Finish the test!')
 
 if __name__ == '__main__':
 	unittest.main(exit=False, warnings='ignore')
--- manage.py
+++ manage.py
@@ -3,8 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
+	print(sys.argv)
+	if len(sys.argv) == 1:
+		sys.argv = ['manage.py', 'test']; print('	 - sys.argv override: ' + str(sys.argv))
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
-    from django.core.management import execute_from_command_line
+	from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+	execute_from_command_line(sys.argv)
