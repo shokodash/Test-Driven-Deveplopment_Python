9a5abaf95fb56b2a087f8d2a61470bce3ebac568
Diamantan <shokolp@gmail.com>
2016-09-30 22:46:49 +0200

    testing a POST request

--- lists/tests.py
+++ lists/tests.py
@@ -15,5 +15,17 @@ class SmokeTest(TestCase):
 		request = HttpRequest()											# <class 'django.http.request.HttpRequest'>
 		response = home_page(request)									# <class 'django.http.response.HttpResponse'>
 		expected_html = render_to_string('home.html')					# <class 'django.utils.safestring.SafeText'>
-		content_decode = response.content.decode()						# <class 'str'>
-		self.assertEqual(content_decode, expected_html)
+		responded_html = response.content.decode()						# <class 'str'>
+		self.assertEqual(responded_html, expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()											# request.method = None
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'					# print(type(request.POST)) -> <class 'django.http.request.QueryDict'>
+
+		response = home_page(request)									# <class 'django.http.response.HttpResponse'>
+		expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+		responded_html = response.content.decode()
+
+		self.assertIn('A new list item', responded_html)
+		self.assertEqual(responded_html, expected_html)
--- manage.py
+++ manage.py
@@ -3,9 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-	print(sys.argv)
-	if len(sys.argv) == 1:
-		sys.argv = ['manage.py', 'test']; print('	 - sys.argv override: ' + str(sys.argv))
+	# print(sys.argv)
+	# if len(sys.argv) == 1:
+	# 	sys.argv = ['manage.py', 'test']; print('	 - sys.argv override: ' + str(sys.argv))
+	
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
 	from django.core.management import execute_from_command_line
