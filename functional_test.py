04c4d0e8c1882255d9f711ba8a358461baff857c
Diamantan <shokolp@gmail.com>
2016-09-30 23:00:39 +0200

    refactor with function check_for_row_in_list_table in functional_test.py

--- functional_test.py
+++ functional_test.py
@@ -11,7 +11,12 @@ class NewVistorTest(unittest.TestCase):
 
 	def tearDown(self):
 		self.browser.quit()
-		
+	
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		# Edith has heard about a cool new online to-do app. She goes
@@ -34,13 +39,7 @@ class NewVistorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 	# When she hits enter, the page updates, and now the page lists
 	# "1: Buy peacock feathers" as an item in a to-do list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		# isit_in_rows = (row.text == '1: Buy peacock feathers' for row in rows)
-		# self.assertTrue(any(isit_in_rows), 
-		# 	"New to-do item did not appear in the table -- its text was: %s\n"%(table.text,))
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-
+		self.check_for_row_in_list_table('Buy peacock feathers')
 
 	# There is still a text box inviting her to add another item. She
 	# enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -49,10 +48,8 @@ class NewVistorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 	# The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
