719999a202be4ce5591f70f11de324f4bbdc2e93
Diamantan <shokolp@gmail.com>
2016-10-06 13:32:14 +0200

    first steps of FT for layout + style

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -35,53 +35,53 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('To-Do', h1_text)
 
 
-    # She is invited to enter a to-do item straight away
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-    # She types "Buy peacock feathers" into a text box (Edith's hobby
-    # is tying fly-fishing lures)
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
         self.assertIn(r'localhost:80', self.browser.current_url)
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)              ## a lot of things happen here, which involve home.html, url.py, views.py, and another request as a result of redirect
 
-    # When she hits enter, she is taken to a new URL,
-    # and now the page lists "1: Buy peacock feathers" as an item in a to-do list table
+        # When she hits enter, she is taken to a new URL,
+        # and now the page lists "1: Buy peacock feathers" as an item in a to-do list table
         edith_list_url = self.browser.current_url
         logg(edith_list_url, 'edith_list_url')
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-    # There is still a text box inviting her to add another item. She
-    # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         self.assertIn('lists/1/', self.browser.current_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-    # The page updates again, and now shows both items on her list
+        # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-    ###################################################################
-    # Now a newuser, Francis comes along
+        ###################################################################
+        # Now a newuser, Francis comes along
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-    # Francis visits the home page.  There is no sign of Edith's list
+        # Francis visits the home page.  There is no sign of Edith's list
         self.browser.get(self.live_server_url)                          ## self.live_server_url -> http://localhost:8081
         body_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', body_text)
         self.assertNotIn('make a fly', body_text)
 
-    # Francis starts a new list by entering a new item. He
-    # is less interesting than Edith...
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
-    # Francis gets his own unique url
+        # Francis gets his own unique url
         self.assertIn('lists/2', self.browser.current_url)
         francis_list_url = self.browser.current_url
         logg(francis_list_url, 'francis_list_url')
@@ -89,7 +89,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertRegex(francis_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy milk')
 
-    # Again there is no trace of Edith's list
+        # Again there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
@@ -104,7 +104,15 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024,768)
 
-    # She notices the input box is nively centered
+        # She notices the input box is nively centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(inputbox.location['x']
                 + inputbox.size['width'] / 2, 512, delta=5)
+        # She starts a new list and sees the input is nicely centered
+        # there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5)
