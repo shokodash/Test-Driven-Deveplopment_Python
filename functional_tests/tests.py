30b9887f5901d881ebdab15f5b1f71702c45846c
Diamantan <shokolp@gmail.com>
2016-10-08 09:53:48 +0200

    remove extreme asserts

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -66,7 +66,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        self.assertIn(r'localhost:80', self.browser.current_url)
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)              ## a lot of things happen here, which involve home.html, url.py, views.py, and another request as a result of redirect
 
@@ -78,7 +77,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.assertIn('lists/1/', self.browser.current_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
@@ -106,7 +104,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # Francis gets his own unique url
-        self.assertIn('lists/2', self.browser.current_url)
         francis_list_url = self.browser.current_url
         self.assertNotEqual(francis_list_url, edith_list_url)
         self.assertRegex(francis_list_url, '/lists/.+')
